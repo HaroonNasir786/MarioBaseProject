504054ad776cc2d2eae590ecefa97f1c9a2c9a5f
Haroon Nasir <59843420+HaroonNasir786@users.noreply.github.com>
2021-02-20 19:44:27 +0000

    Revert "Screen Manager"
    This reverts commit 4cb97bd4ce39663644cf4d6a05f02527ff20f7a7.

--- MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
+++ MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
@@ -1,6 +1,3 @@
-#include "GameScreenManager.h"
-GameScreenManager* game_screen_manager;
-Uint32 g_old_time;
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
@@ -8,7 +5,6 @@ Uint32 g_old_time;
 #include "constants.h"
 #include "Texture2D.h"
 #include "Commons.h"
-
 using namespace std;
 //Globals
 SDL_Window* g_window = nullptr;
@@ -18,19 +14,15 @@ bool InitSDL();
 void CLoseSDL();
 bool Update();
 void Render();
-//Texture2D* g_texture = nullptr;
+Texture2D* g_texture = nullptr;
 
 int main(int argc, char* args[])
 {
 	//check if sdl was setup correctly
 	if (InitSDL())
 	{
-		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
-		//set the time
-		g_old_time = SDL_GetTicks();
-
 		//flag to check if we wish to quit
-		bool quit = true;
+		bool quit = false;
 
 		//Game Loop
 		while (!quit)
@@ -92,12 +84,15 @@ bool InitSDL()
 	}
 
 	//Load the background texture
-	/*g_texture = new Texture2D(g_renderer);
+	g_texture = new Texture2D(g_renderer);
 
 	if (!g_texture->LoadFromFile("Images/test.bmp"))
 	{
 		return false;
-	}*/
+	}
+
+
+
 }
 
 void CLoseSDL()
@@ -117,12 +112,9 @@ void CLoseSDL()
 	g_renderer = nullptr;
 
 	//release the texture
-	/*delete g_texture;
-	g_texture = nullptr;*/
+	delete g_texture;
+	g_texture = nullptr;
 
-	//destroy the game screen manager
-	delete game_screen_manager;
-	game_screen_manager = nullptr;
 
 
 }
@@ -130,14 +122,8 @@ void CLoseSDL()
 bool Update()
 {
 	//Event handler
-	SDL_Event e{};
-	SDL_PollEvent;
-	
+	SDL_Event e;
 
-	Uint32 new_time = SDL_GetTicks();
-	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
-	g_old_time = new_time;
-	
 	//get events
 	SDL_PollEvent(&e);
 	//handle the events
@@ -158,9 +144,7 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_renderer);
 
-	/*g_texture->Render(Vector2D(), SDL_FLIP_NONE);*/
-
-	game_screen_manager->Render();
+	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
 
 	//update the screen
 	SDL_RenderPresent(g_renderer);
