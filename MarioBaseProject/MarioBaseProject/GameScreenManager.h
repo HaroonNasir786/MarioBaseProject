18cfaa1143fbed95ee049c5b2de26c4cb14425e7
Haroon Nasir <59843420+HaroonNasir786@users.noreply.github.com>
2021-02-25 22:51:48 +0000

    Tutorial 6
    Completed

---
+++ MarioBaseProject/MarioBaseProject/GameScreenManager.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Commons.h"
+#include "GameScreen.h"
+class GameScreen;
+
+class GameScreenManager
+{
+public:
+
+	GameScreenManager(SDL_Renderer* renderer, SCREENS startScreen);
+	~GameScreenManager();
+
+	void Render();
+	void Update(float deltaTime, SDL_Event e);
+
+	void ChangeScreen(SCREENS new_screen);
+
+
+private:
+
+	SDL_Renderer* m_renderer;
+	GameScreen* m_current_screen;
+
+};
+
+
+
--- MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
+++ MarioBaseProject/MarioBaseProject/MarioBaseProject.cpp
@@ -5,6 +5,7 @@
 #include "constants.h"
 #include "Texture2D.h"
 #include "Commons.h"
+#include "GameScreenManager.h"
 using namespace std;
 //Globals
 SDL_Window* g_window = nullptr;
@@ -14,14 +15,19 @@ bool InitSDL();
 void CLoseSDL();
 bool Update();
 void Render();
-Texture2D* g_texture = nullptr;
+GameScreenManager* game_screen_manager;
+Uint32 g_old_time;
+
 
 int main(int argc, char* args[])
 {
 	//check if sdl was setup correctly
 	if (InitSDL())
 	{
-		//flag to check if we wish to quit
+		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
+		//set the time
+		g_old_time = SDL_GetTicks();
+
 		bool quit = false;
 
 		//Game Loop
@@ -82,17 +88,6 @@ bool InitSDL()
 		cout << "Renderer could not initialise. Error: " << SDL_GetError();
 		return false;
 	}
-
-	//Load the background texture
-	g_texture = new Texture2D(g_renderer);
-
-	if (!g_texture->LoadFromFile("Images/test.bmp"))
-	{
-		return false;
-	}
-
-
-
 }
 
 void CLoseSDL()
@@ -105,22 +100,21 @@ void CLoseSDL()
 	IMG_Quit();
 	SDL_Quit();
 
-	//clear the texture
-
 	//release the renderer
 	SDL_DestroyRenderer(g_renderer);
 	g_renderer = nullptr;
 
-	//release the texture
-	delete g_texture;
-	g_texture = nullptr;
-
+	//destroy the game screen manager
+	delete game_screen_manager;
+	game_screen_manager = nullptr;
 
 
 }
 
 bool Update()
 {
+	Uint32 new_time = SDL_GetTicks();
+
 	//Event handler
 	SDL_Event e;
 
@@ -135,6 +129,10 @@ bool Update()
 		break;
 	}
 
+	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
+
+	g_old_time = new_time;
+
 	return false;
 }
 
@@ -144,7 +142,7 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_renderer);
 
-	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	game_screen_manager->Render();
 
 	//update the screen
 	SDL_RenderPresent(g_renderer);
